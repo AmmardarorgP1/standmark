f8aa2b4ea6c2dbb13df35248f3d9f074b6cea0e7
Ammar Dar <137186556+AmmardarorgP1@users.noreply.github.com>
2026-07-26 14:11:29 +0500

    Add password hashing, signup service,controller, and error handler

---
+++ src/controllers/authService.js
@@ -0,0 +1,24 @@
+const authService = require('../services/authService');
+
+
+
+
+const signup = async(req,res,next)=>{
+    try{
+        const result = await authService.signUp(req.body);
+        res.status(201).json(
+            {
+                success:true,
+                message:'Account Created Successfully',   
+                data: result,
+            }
+        );
+    }catch(error)
+    {
+        next(error);
+    }
+};
+
+
+module.exports = {signUp};
+
---
+++ src/services/authService.js
@@ -0,0 +1,28 @@
+const User = required('../models/User');
+
+
+
+const signUp = async ({name,email,password})=>{
+
+
+    const existingUser = await User.findOne({email});
+
+    if(existingUser)
+    {
+        const error = new Error('Email already registered');
+        error.statusCode = 409;   // shows conflict in registration
+        throw error;
+    }
+
+
+    const user = await User.create({name,email,password});
+
+    return {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+    };
+};
+
+
+module.exports = {signUp};
